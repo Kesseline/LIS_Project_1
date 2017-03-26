044389f43bcfc5a4d47fc466732dc582abbc5225
Kesseline <freym@student.ethz.ch>
2017-03-26 15:06:36 +0200

    First Submission with polynomial kernel

---
+++ Submissions/generatePolyKernelRidgeSub.m
@@ -0,0 +1,20 @@
+% Submission leads to error of 48.9
+
+load('../data/train.mat');
+load('../data/test.mat');
+load('../data/indices.mat');
+addpath(genpath('..'));
+filename = 'polykernridgesub.csv';
+
+lambda = 1000;
+p = 3;
+
+kern = @( x1, x2 )( (x1 * x2' + 1 )^1 );
+ytest = kernridgereg(X,y,X_test, kern, lambda);
+res = [indices, ytest];
+
+fid = fopen(filename, 'w');
+fprintf(fid, 'Id,y\n');
+fclose(fid);
+dlmwrite(filename,res,'-append','precision','%.4f');
+
