75eefe10b68ae57b68273b92f96bd233d2163302
Kesseline <freym@student.ethz.ch>
2017-03-26 15:18:21 +0200

    "Revised Submission"

--- Submissions/generatePolyKernelRidgeSub.m
+++ Submissions/generatePolyKernelRidgeSub.m
@@ -1,4 +1,4 @@
-% Submission leads to error of 48.9
+% Submission leads to error of 17.15
 
 load('../data/train.mat');
 load('../data/test.mat');
@@ -9,7 +9,7 @@ filename = 'polykernridgesub.csv';
 lambda = 1000;
 p = 3;
 
-kern = @( x1, x2 )( (x1 * x2' + 1 )^1 );
+kern = @( x1, x2 )( (x1 * x2' + 1 )^p );
 ytest = kernridgereg(X,y,X_test, kern, lambda);
 res = [indices, ytest];
 
